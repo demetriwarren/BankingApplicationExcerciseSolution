cb2a8d4b643d8e212a0caa5e2bf815b9718dcd29
Demetri Warren <dwarren@tql.com>
2024-06-14 15:58:27 -0400

    Add project files.

---
+++ BankingApplicationExcercise/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankingApplicationExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public int AccountId { get; set; };
+        public string Description { get; set; };
+        public decimal Balance { get; set; };
+        }
+        
+
+    }
+}
