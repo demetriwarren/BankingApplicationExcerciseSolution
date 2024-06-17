5d5b34104f9de007c05b97efb648fb72b17b3b42
Demetri Warren <dwarren@tql.com>
2024-06-17 14:15:39 -0400

    updated notes

---
+++ BankingApplicationExcercise/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplicationExcercise
+{
+   public class Account                                     // Class
+    {
+        public int AccountId { get; set; } = 0;             //properties
+        public string Description { get; set; } = string.Empty;
+        public decimal Balance { get; set; } = 0;
+
+        public bool Deposit(decimal Amount)                 //methods - the thing in the parenthesis says that you must pass something thru this when referencing.
+        {
+            if (Amount <= 0)
+            {
+                Console.WriteLine("Deposit amount cannot be zero or negative");
+                return false;
+            }
+            Balance += Amount;                              // balance is an instance of the property: Balance
+            return true;
+        }
+        public bool Withdraw(decimal Amount)
+        {
+            if (Amount <= 0)
+            {                                               //everything in this curly bracket is what the Method is going to do when its called/ran
+                Console.WriteLine("Withdrawal amount cannot be zero or negative");
+                return false;
+            }
+            if (Amount > Balance) { Console.WriteLine("Insufficient funds.");
+                return false;
+            }
+            Balance -= Amount;
+            return true;
+        }
+        public bool Transfer(decimal Amount, Account accountx) 
+        {
+            var success = Withdraw(Amount);
+            if (success == true)
+            {
+                accountx.Deposit(Amount);
+            }
+            return true;
+        }
+        
+    }
+}
--- BankingApplicationExcercise/Program.cs
+++ BankingApplicationExcercise/Program.cs
@@ -4,9 +4,27 @@
     {
         static void Main(string[] args)
         {
-            public int AccountId { get; set; };
-        public string Description { get; set; };
-        public decimal Balance { get; set; };
+            Account A1 = new Account();                             //creating the different accounts before being able to reference them
+            Account A2 = new Account();
+
+            Console.WriteLine($"Balance is {A1.Balance:C}");        //using A1.xxx says to do the referenced Method to the called account
+            A1.Deposit(500);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Withdraw(250);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Withdraw(260);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Withdraw(50);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Deposit(750);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Deposit(-500);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+            A1.Withdraw(-100);
+            Console.WriteLine($"Balance is {A1.Balance:C}");
+
+            A1.Transfer(100, A2);                                   // transfers from A1 to A2
+
         }
         
 
